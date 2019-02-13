cfc40975abf3b6a42424ab9bf063b6131a9a5b16
Janis Zisenis <janis.zisenis@hotmail.de>
2019-02-13 20:50:41 +0100

    Introduces UserInput class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,19 +11,43 @@ public class Main {
     public static void main(String[] args) {
         FieldPrinter.print(field);
 
-        scanInput();
+        UserInput in = scanInput();
 
         FieldPrinter.print(field);
     }
 
-    private static void scanInput() {
+    private static UserInput scanInput() {
         Scanner in = makeScanner();
         int row = in.nextInt();
         int col = in.nextInt();
+
+        return makeUserInput(row, col);
+    }
+
+    private static UserInput makeUserInput(int row, int col) {
+        return new UserInput(row, col);
     }
 
     private static Scanner makeScanner() {
         return new Scanner(System.in);
+    }
+
+    private static class UserInput {
+        private int row;
+        private int column;
+
+        public UserInput(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
     }
 
 }
