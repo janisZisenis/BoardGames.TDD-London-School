9d74cb589ef731083c56e07a6d1d0c00c653fd46
Janis Zisenis <janis.zisenis@hotmail.de>
2019-02-21 17:54:11 +0100

    doOver Field marking tests

--- src/com/company/TicTacToeBoard/TicTacToeBoard.java
+++ src/com/company/TicTacToeBoard/TicTacToeBoard.java
@@ -4,9 +4,6 @@ import com.company.IsOnBoardValidatorImp.Field;
 
 public class TicTacToeBoard {
 
-    private Field xField = null;
-    private Field oField = null;
-
     public boolean exists(Field field) {
         if(isOutOfBounds(field.getRow()) || isOutOfBounds(field.getColumn()))
             return false;
@@ -18,36 +15,4 @@ public class TicTacToeBoard {
         return row < 0 || row > 2;
     }
 
-    public boolean isEmpty(Field field) {
-        if(xField != null)
-            if(!field.equals(xField))
-                return true;
-        if(oField != null)
-            if(!field.equals(oField))
-                return true;
-
-        return xField == null && oField == null;
-    }
-
-    public void markX(Field field) {
-        xField = field;
-    }
-
-    public boolean isX(Field field) {
-        if(xField == null)
-            return false;
-
-        return field.equals(xField);
-    }
-
-    public boolean isO(Field field) {
-        if(oField == null)
-            return false;
-
-        return field.equals(oField);
-    }
-
-    public void markO(Field field) {
-        oField = field;
-    }
 }
---
+++ test/com/company/TicTacToeBoard/FieldMarkingTest.java
@@ -0,0 +1,16 @@
+package com.company.TicTacToeBoard;
+
+import com.company.IsOnBoardValidatorImp.Field;
+import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.MarkBits;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FieldMarkingTest {
+
+    @Test
+    void NextTestGoesHere() {
+        assertTrue(false);
+    }
+
+}
