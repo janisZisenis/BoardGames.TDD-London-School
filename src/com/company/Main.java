35b2b02df01fb705c14f1c4bd4a1b751af39c787
Janis Zisenis <janis.zisenis@hotmail.de>
2019-02-23 18:44:22 +0100

    changes board in main to Mark[][]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,15 @@ package com.company;
 
 import com.company.ConsoleInputPrompter.ConsoleInputPrompter;
 import com.company.DefaultInputGenerator.DefaultInputGenerator;
+import com.company.IsOnBoardValidatorImp.Field;
 import com.company.IsOnBoardValidatorImp.IsOnBoardValidatorImp;
 import com.company.OnBoardInputGenerator.OnBoardInputGenerator;
+import com.company.TicTacToeBoard.Mark;
 
 public class Main {
-    static char[][] field = {   {'.', '.', '.'},
-                                {'.', '.', '.'},
-                                {'.', '.', '.'}   };
+    static Mark[][] board = {   {null, null, null},
+                                {null, null, null},
+                                {null, null, null}   };
 
     public static void main(String[] args) {
         ConsoleInputPrompter prompter = new ConsoleInputPrompter();
@@ -16,44 +18,78 @@ public class Main {
         IsOnBoardValidatorImp onBoardValidator = new IsOnBoardValidatorImp(null);
         OnBoardInputGenerator onBoardGenerator = new OnBoardInputGenerator(generator, onBoardValidator);
 
-        BoardPrinter.print(field);
+        print(board);
 
         UserInput in = generator.generateInput();
-        while(isOutOfBounds(in)) {
+        while(exists(in)) {
             System.out.println("The inserted input is out of bounds. Please insert again!");
             in = generator.generateInput();
         }
         applyX(in);
 
-        while(isOutOfBounds(in)) {
+        while(exists(in)) {
             in = generator.generateInput();
         }
-        BoardPrinter.print(field);
+        print(board);
 
         in = generator.generateInput();
-        while(isOutOfBounds(in)) {
+        while(exists(in)) {
             System.out.println("The inserted input is out of bounds. Please insert again!");
             in = generator.generateInput();
         }
         applyO(in);
 
-        BoardPrinter.print(field);
+        print(board);
     }
 
-    private static boolean isOutOfBounds(UserInput in) {
+    //Board
+    private static boolean exists(UserInput in) {
         return in.getColumn() < 0 || in.getColumn() > 2 || in.getRow() < 0 || in.getRow() > 2;
     }
 
+    private static boolean isEmpty(int row, int column) {
+        return board[row][column] == null;
+    }
+
+    private static void mark(Mark m, Field f) {
+        board[f.getRow()][f.getColumn()] = m;
+    }
+
+    //Player
     private static void applyX(UserInput in) {
-        applyFor('X', in);
+        Field f = new Field(in.getRow(), in.getColumn());
+        mark(Mark.X, f);
     }
 
     private static void applyO(UserInput in) {
-        applyFor('O', in);
+        Field f = new Field(in.getRow(), in.getColumn());
+        mark(Mark.O, f);
+
     }
 
-    private static void applyFor(char player, UserInput in) {
-        field[in.getRow()][in.getColumn()] = player;
+
+    //Printer
+    public static void print(Mark[][] board) {
+        for(int row = 0; row < 3; row++) {
+            printRow(row, board);
+        }
+    }
+
+    public static void printRow(int row, Mark[][] board) {
+        for(int col = 0; col < 3; col++) {
+            if(isEmpty(row, col)) {
+                System.out.print('.');
+            }
+            else {
+                char c = map(board[row][col]);
+                System.out.print(c);
+            }
+        }
+        System.out.print('\n');
+    }
+
+    private static char map(Mark m) {
+        return (m == Mark.X) ? 'X' : 'O';
     }
 
 }
