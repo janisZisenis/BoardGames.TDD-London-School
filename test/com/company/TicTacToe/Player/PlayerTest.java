c9741710d006505b0ce21564f26df95228d2a2be
Janis Zisenis <janis.zisenis@hotmail.de>
2019-02-26 19:00:23 +0100

    refactors JohnTest renames JohnTest in PlayerTest

--- test/com/company/TicTacToe/Player/PlayerTest.java
+++ test/com/company/TicTacToe/Player/PlayerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class JohnTest {
+public class PlayerTest {
 
     InputGeneratorStub generator = new InputGeneratorStub();
     MarkFieldServiceMock markService = new MarkFieldServiceMock();
@@ -17,12 +17,8 @@ public class JohnTest {
 
     @Test
     void IfInputHasRow1AndColumn2_JohnShouldMarkFieldRow1Column2WithHisMark() {
-        makePlayerIsJohn();
-
-        Input in = new Input(1, 2);
-        makeGeneratorReturns(in);
-        Field f = new Field(1, 2);
-        makeMarkServiceExpects(f, Mark.John);
+        makeJohnMarksRow1Column1();
+        makeMockExpectsJohnMarksRow1Column1();
 
         sut.play();
 
@@ -31,11 +27,8 @@ public class JohnTest {
 
     @Test
     void IfInputHasRow2AndColumn1_JohnShouldMarkFieldRow2Column1WithHisMark() {
-        makePlayerIsJohn();
-        Input in = new Input(2, 1);
-        makeGeneratorReturns(in);
-        Field f = new Field(2, 1);
-        makeMarkServiceExpects(f, Mark.John);
+        makeJohnMarksRow2Column0();
+        makeMockExpectsJohnMarksRow2Column0();
 
         sut.play();
 
@@ -44,25 +37,12 @@ public class JohnTest {
 
     @Test
     void IfInputHasRow1AndColumn2_HaleyShouldMarkFieldRow1Column2WithHerMark() {
-        makePlayerIsHaley();
-        Input in = new Input(1, 2);
-        makeGeneratorReturns(in);
-        Field f = new Field(1, 2);
-        makeMarkServiceExpects(f, Mark.Haley);
+        makeHaleyMarksRow1Column1();
+        makeMockExpectsHaleyMarksRow1Column1();
 
         sut.play();
 
         markService.verifyAll();
-    }
-
-
-    private void makeMarkServiceExpects(Field f, Mark p) {
-        markService.expectFieldWasMarkedWith(p, f);
-    }
-
-    private void makeGeneratorReturns(Input in) {
-        Input[] inputs = { in };
-        generator.setUserInputs(inputs);
     }
 
     private void makePlayerIsJohn() {
@@ -81,5 +61,50 @@ public class JohnTest {
 
     private PlayerConfig makePlayerConfig(Mark mark) {
         return new PlayerConfig(generator, markService, mark);
+    }
+
+
+    private void makeJohnMarksRow1Column1() {
+        makePlayerIsJohn();
+        makeGeneratorReturnsRow1Column1();
+    }
+
+    private void makeJohnMarksRow2Column0() {
+        makePlayerIsJohn();
+        makeGeneratorReturnsRow2Column0();
+    }
+
+    private void makeHaleyMarksRow1Column1() {
+        makePlayerIsHaley();
+        makeGeneratorReturnsRow1Column1();
+    }
+
+
+    private void makeGeneratorReturnsRow2Column0() {
+        Input in = new Input(2, 0);
+        Input[] inputs = { in };
+        generator.setUserInputs(inputs);
+    }
+
+    private void makeGeneratorReturnsRow1Column1() {
+        Input in = new Input(1, 1);
+        Input[] inputs = { in };
+        generator.setUserInputs(inputs);
+    }
+
+
+    private void makeMockExpectsJohnMarksRow1Column1() {
+        Field f = new Field(1, 1);
+        markService.expectFieldWasMarkedWith(Mark.John, f);
+    }
+
+    private void makeMockExpectsHaleyMarksRow1Column1() {
+        Field f = new Field(1, 1);
+        markService.expectFieldWasMarkedWith(Mark.Haley, f);
+    }
+
+    private void makeMockExpectsJohnMarksRow2Column0() {
+        Field f = new Field(2, 0);
+        markService.expectFieldWasMarkedWith(Mark.John, f);
     }
 }
