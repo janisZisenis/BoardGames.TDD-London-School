f11f1bf9361081e361bc798b9ad6bf3fe4303dd2
Janis Zisenis <janis.zisenis@hotmail.de>
2019-04-26 18:34:57 +0200

    adds ConsoleTurnMessageView, uses VerboseTwoPlayerTurn in Main

--- src/com/company/App/Main.java
+++ src/com/company/App/Main.java
@@ -1,15 +1,13 @@
 package com.company.App;
 
 import com.company.CLI.View.TicTacToeView.TicTacToeConsoleView;
-import com.company.Data.Mark;
 import com.company.Model.Board.Board;
 import com.company.Model.Board.ObservableBoard.ObservableBoard;
 import com.company.Model.GameEvaluation.EquallyMarkedLineEvaluator.EquallyMarkedLineEvaluator;
 import com.company.Model.GameEvaluation.GameEvaluator.GameEvaluator;
 import com.company.Model.GameEvaluation.GameEvaluator.LineEvaluator;
 import com.company.Model.GameEvaluation.HumbleLineProvider.HumbleLineProvider;
-import com.company.Model.GameLoop.GameOverRule;
-import com.company.Model.GameLoop.TwoPlayerTurn.Player;
+import com.company.Model.GameLoop.GameLoop;
 import com.company.Presentation.BoardPresenter.BoardPresenter;
 import com.company.Presentation.BoardPresenter.WinningLineProvider;
 import com.company.Presentation.LeaveTaker.LeaveTaker;
@@ -51,25 +49,10 @@ public class Main {
         showSalutation();
         view.display(board);
 
-        Player current;
-        Player john = current = factory.makeComputerPlayer(board, Mark.John);
-        Player haley = factory.makeComputerPlayer(board, Mark.Haley);
-        GameOverRule rule = factory.makeTicTacToeGameOverRule(board);
-
-        mapping.put(john, "X");
-        mapping.put(haley, "O");
-
-        while(!rule.isGameOver()) {
-            showTurnMessageFor(current);
-            current.playMove();
-            current = (current == john) ? haley : john;
-        }
+        GameLoop loop = factory.makeGameLoop(board);
+        loop.run();
 
         leaveTaker.showLeaveTaking();
-    }
-
-    private static void showTurnMessageFor(Object player) {
-        System.out.println(mapping.get(player) + ", it's your turn!");
     }
 
     private static void showSalutation() {
