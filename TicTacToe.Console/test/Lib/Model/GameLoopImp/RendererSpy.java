273542d42192e3b273c8723406ac22810e1437f7
Janis Zisenis <janis.zisenis@hotmail.de>
2019-04-28 14:21:19 +0200

    deletes hasRendered method from RendererSpy

--- TicTacToe.Console/test/Lib/Model/GameLoopImp/RendererSpy.java
+++ TicTacToe.Console/test/Lib/Model/GameLoopImp/RendererSpy.java
@@ -2,15 +2,9 @@ package Lib.Model.GameLoopImp;
 
 public class RendererSpy implements Renderer {
 
-    private boolean didRender = false;
     private int timesRendered = 0;
 
-    public boolean hasRendered() {
-        return didRender;
-    }
-
     public void render() {
-        didRender = true;
         timesRendered++;
     }
 
