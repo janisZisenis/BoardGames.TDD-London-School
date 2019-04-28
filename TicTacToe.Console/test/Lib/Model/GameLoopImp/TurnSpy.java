fc63e6e71541fea2110ec85c008d39141ae3d2e4
Janis Zisenis <janis.zisenis@hotmail.de>
2019-04-28 14:21:59 +0200

    deletes hasPlayed method from TurnSpy

--- TicTacToe.Console/test/Lib/Model/GameLoopImp/TurnSpy.java
+++ TicTacToe.Console/test/Lib/Model/GameLoopImp/TurnSpy.java
@@ -2,16 +2,10 @@ package Lib.Model.GameLoopImp;
 
 public class TurnSpy implements Turn {
 
-    private boolean didPlay = false;
     private int timesPlayed = 0;
 
     public void play() {
-        didPlay = true;
         timesPlayed++;
-    }
-
-    public boolean hasPlayed() {
-        return didPlay;
     }
 
     public int getPlayedTimes() {
