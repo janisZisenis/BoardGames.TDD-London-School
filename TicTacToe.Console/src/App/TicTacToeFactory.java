02e43aa874d105a23e91d48b79dcdc3821152d5e
Janis Zisenis <janis.zisenis@hotmail.de>
2019-05-05 19:16:53 +0200

    implements minimax and makes first player a computer player in Console

--- TicTacToe.Console/src/App/TicTacToeFactory.java
+++ TicTacToe.Console/src/App/TicTacToeFactory.java
@@ -15,6 +15,7 @@ import Domain.GameEvaluation.GameEvaluator.LineProvider;
 import Domain.GameEvaluation.HumbleLineProvider.HumbleLineProvider;
 import Domain.InputGenerators.AlertingInputGenerator.AlertingInputGenerator;
 import Domain.InputGenerators.AlertingInputGenerator.InputValidator;
+import Domain.InputGenerators.MinimaxInputGenerator.MinimaxInputGenerator;
 import Domain.InputGenerators.RandomInputGenerator.RandomInputGenerator;
 import Domain.InputGenerators.ValidatingInputGenerator.ValidatingInputGenerator;
 import Domain.InputRules.CompositeInputRule.CompositeInputRule;
@@ -103,7 +104,7 @@ public class TicTacToeFactory {
 
     private GameMessenger makeGameMessenger(Board board) {
         WinnerProvider provider = makeGameEvaluator(board);
-        MarkToStringMapper messageMapper = new MarkToMessageMapper(computerWinMessage, humanWinMessage);
+        MarkToStringMapper messageMapper = new MarkToMessageMapper(humanWinMessage, computerWinMessage);
         WinnerMessageProviderImp winnerMessageProvider = new WinnerMessageProviderImp(provider, messageMapper);
         GameOverMessageProviderImp goMessageProvider = new GameOverMessageProviderImp(winnerMessageProvider, drawMessage);
         return new GameMessengerImp(messenger, goMessageProvider, salutation);
@@ -172,12 +173,12 @@ public class TicTacToeFactory {
 
 
     private Turn makeTurn(Board board) {
-        Player john = makeComputerPlayer(board, Mark.John);
-        Player haley = makeHumanPlayer(board, Mark.Haley);
+        Player john = makeHumanPlayer(board, Mark.John);
+        Player haley = makeComputerPlayer(board, Mark.Haley);
 
         ObjectToMessageMapper objectMapper = new ObjectToMessageMapper();
-        objectMapper.register(john, computerTurnMessage);
-        objectMapper.register(haley, humanTurnMessage);
+        objectMapper.register(john, humanTurnMessage);
+        objectMapper.register(haley, computerTurnMessage);
         TurnMessenger messenger = makeTurnMessenger(objectMapper);
 
         return new MessagingTwoPlayerTurn(john, haley, messenger);
@@ -189,7 +190,7 @@ public class TicTacToeFactory {
     }
 
     private Player makeComputerPlayer(Board board, Mark m) {
-        InputGenerator computerGenerator = makeComputerInputGenerator(board);
+        InputGenerator computerGenerator = makeComputerInputGenerator(board, m);
         return makePlayer(board, computerGenerator, m);
     }
 
@@ -199,10 +200,14 @@ public class TicTacToeFactory {
         return new MessagingPlayer(context, messenger);
     }
 
-    private InputGenerator makeComputerInputGenerator(Board board) {
+    private InputGenerator makeComputerInputGenerator(Board board, Mark m) {
         InputRule inputRule = makeInputRule(board);
-        InputGenerator randomGenerator = makeRandomInputGenerator();
+        InputGenerator randomGenerator = makeMinimaxInputGenerator(board, m);
         return new ValidatingInputGenerator(randomGenerator, inputRule);
+    }
+
+    private InputGenerator makeMinimaxInputGenerator(Board board, Mark m) {
+        return new MinimaxInputGenerator(board, m);
     }
 
     private RandomInputGenerator makeRandomInputGenerator() {
--- TicTacToe.SequentialGui/src/App/TicTacToeFactory.java
+++ TicTacToe.SequentialGui/src/App/TicTacToeFactory.java
@@ -13,6 +13,7 @@ import Domain.GameEvaluation.GameEvaluator.LineProvider;
 import Domain.GameEvaluation.HumbleLineProvider.HumbleLineProvider;
 import Domain.InputGenerators.AlertingInputGenerator.AlertingInputGenerator;
 import Domain.InputGenerators.AlertingInputGenerator.InputValidator;
+import Domain.InputGenerators.MinimaxInputGenerator.MinimaxInputGenerator;
 import Domain.InputGenerators.RandomInputGenerator.RandomInputGenerator;
 import Domain.InputGenerators.ValidatingInputGenerator.ValidatingInputGenerator;
 import Domain.InputRules.CompositeInputRule.CompositeInputRule;
@@ -187,7 +188,7 @@ public class TicTacToeFactory {
     }
 
     private Player makeComputerPlayer(Board board, Mark m) {
-        InputGenerator computerGenerator = makeComputerInputGenerator(board);
+        InputGenerator computerGenerator = makeComputerInputGenerator(board, m);
         return makePlayer(board, computerGenerator, m);
     }
 
@@ -197,10 +198,14 @@ public class TicTacToeFactory {
         return new MessagingPlayer(context, messenger);
     }
 
-    private InputGenerator makeComputerInputGenerator(Board board) {
+    private InputGenerator makeComputerInputGenerator(Board board, Mark m) {
         InputRule inputRule = makeInputRule(board);
-        InputGenerator randomGenerator = makeRandomInputGenerator();
+        InputGenerator randomGenerator = makeMinimaxInputGenerator(board, m);
         return new ValidatingInputGenerator(randomGenerator, inputRule);
+    }
+
+    private InputGenerator makeMinimaxInputGenerator(Board board, Mark m) {
+        return new MinimaxInputGenerator(board, m);
     }
 
     private RandomInputGenerator makeRandomInputGenerator() {
