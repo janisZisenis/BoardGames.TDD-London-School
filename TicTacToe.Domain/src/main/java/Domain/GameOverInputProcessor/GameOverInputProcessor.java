330a2ceb95cc2e1931223782d453195c4782d40b
Janis Zisenis <janis.zisenis@hotmail.de>
2019-05-13 00:06:56 +0200

    adds tests for GameOverInputProcessor

---
+++ TicTacToe.Domain/src/main/java/Domain/GameOverInputProcessor/GameOverInputProcessor.java
@@ -0,0 +1,22 @@
+package Domain.GameOverInputProcessor;
+
+import InputGeneration.Input.Input;
+import InputGeneration.InputProcessor;
+import SequentialGaming.GameFacade.GameOverRule;
+
+public class GameOverInputProcessor {
+
+    private final InputProcessor processor;
+    private final GameOverRule rule;
+
+    public GameOverInputProcessor(InputProcessor processor, GameOverRule rule) {
+        this.rule = rule;
+        this.processor = processor;
+    }
+
+    public void process(Input input) {
+        if(!rule.isGameOver())
+            processor.process(input);
+    }
+
+}
