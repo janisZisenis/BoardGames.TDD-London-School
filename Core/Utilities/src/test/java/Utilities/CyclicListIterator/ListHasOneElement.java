20949a987630c756e3e2190daa6484e2de382a1c
Janis Zisenis <janis.zisenis@hotmail.de>
2019-05-15 20:02:28 +0200

    moves Apps in Apps module as submodules

--- Core/Utilities/src/test/java/Utilities/CyclicListIterator/ListHasOneElement.java
+++ Core/Utilities/src/test/java/Utilities/CyclicListIterator/ListHasOneElement.java
@@ -2,6 +2,7 @@ package Utilities.CyclicListIterator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ListHasOneElement {
 
     private Object object = new Object();
-    private List<Object> list = List.of(object);
+    private List<Object> list = Arrays.asList(object);
     private CyclicListIterator<Object> sut = new CyclicListIterator<>(list);
 
     @Test
--- Core/Utilities/src/test/java/Utilities/CyclicListIterator/ListHasTwoElements.java
+++ Core/Utilities/src/test/java/Utilities/CyclicListIterator/ListHasTwoElements.java
@@ -2,6 +2,7 @@ package Utilities.CyclicListIterator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,7 +11,7 @@ public class ListHasTwoElements {
 
     private Object first = new Object();
     private Object second = new Object();
-    private List<Object> list = List.of(first, second);
+    private List<Object> list = Arrays.asList(first, second);
     private CyclicListIterator<Object> sut = new CyclicListIterator<>(list);
 
     @Test
